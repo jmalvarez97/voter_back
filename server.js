8782bd4209690a0ce3cf1bee1ba1bf4845ffb51a
Juan Manuel Alvarez <jualvarez@fi.uba.ar>
2023-03-28 14:32:25 -0300

    cambios en dialect

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL)
+const db = new Sequelize('db_clubes', 'db_clubes_user', process.env.PASSWORD_DB, {
+    host: process.env.HOSTNAME_DB,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: 'true'
+        }
+      }
+  });
 
 module.exports = db;
